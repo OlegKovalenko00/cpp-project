d2417c601e7e950a2308a2261cf1d6c0b1379500
olegus <olkovko64@gmail.com>
2025-12-06 14:49:35 +0300

    add make_metric_from_unix

--- metrics-service/include/metrics.h
+++ metrics-service/include/metrics.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+struct Metric {
+    std::string name;
+    double value;
+    std::chrono::system_clock::time_point timestamp;
+};
+
+Metric make_metric_from_unix(const std::string& name, double value, std::int64_t unix_second);
--- metrics-service/src/metrics.cpp
+++ metrics-service/src/metrics.cpp
@@ -1,1 +1,11 @@
 #include "metrics.h"
+
+Metric make_metric_from_unix(const std::string& name, double value, std::int64_t unix_second) {
+    std::chrono::system_clock::time_point tp = std::chrono::system_clock::time_point{std::chrono::seconds(unix_second)};
+
+    Metric metric;
+    metric.name = name;
+    metric.value = value;
+    metric.timestamp = tp;
+    return metric;
+}

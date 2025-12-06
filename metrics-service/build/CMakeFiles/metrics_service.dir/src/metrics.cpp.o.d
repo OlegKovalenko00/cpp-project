539cac8a9b447b0d74719d04616a66cbeeeeaef0
olegus <olkovko64@gmail.com>
2025-12-06 11:55:44 +0300

    issue 1 sol, Kovalenko Oleg

---
+++ metrics-service/build/CMakeFiles/metrics_service.dir/src/metrics.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/metrics_service.dir/src/metrics.cpp.o: \
+ /home/olozavr/Documents/projects/c++/cpp-project/metrics-service/src/metrics.cpp \
+ /usr/include/stdc-predef.h \
+ /home/olozavr/Documents/projects/c++/cpp-project/metrics-service/include/metrics.h
---
+++ metrics-service/src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Metrics service started" << std::endl;
+}

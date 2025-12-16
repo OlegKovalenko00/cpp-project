444704cf5fc3059c87abfe14b787b6fbd2cd569b
arthast <amtakhunov@edu.hse.ru>
2025-12-16 03:23:45 +0300

    fix bugs our teamlead

--- api-service/src/aggregation.grpc.pb.cc
+++ api-service/src/aggregation.grpc.pb.cc
@@ -15,11 +15,10 @@
 #include <grpcpp/support/method_handler.h>
 #include <grpcpp/impl/rpc_service_method.h>
 #include <grpcpp/support/server_callback.h>
-#include <grpcpp/impl/server_callback_handlers.h>
+#include <grpcpp/impl/codegen/server_callback_handlers.h>
 #include <grpcpp/server_context.h>
 #include <grpcpp/impl/service_type.h>
 #include <grpcpp/support/sync_stream.h>
-#include <grpcpp/ports_def.inc>
 namespace metricsys {
 namespace aggregation {
 
@@ -296,5 +295,4 @@ AggregationService::Service::~Service() {
 
 }  // namespace metricsys
 }  // namespace aggregation
-#include <grpcpp/ports_undef.inc>
 
